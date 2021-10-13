2e93031b237b2df95ff7b815c0303d44729377f2
Luca <averuzmaximuz@gmail.com>
2021-10-14 04:58:26 +0700

    ngerapihin

--- inputan.php
+++ inputan.php
@@ -5,10 +5,10 @@ include "scripts.php";
 
 $nama_pengunjung=$_POST["nama_pengunjung"];
 $pesan=$_POST["pesan"];
-
+$last_name=$_POST["last_name"];
 
 //input kedalam tabel data_inputan
-$sql = "insert into data_inputan (nama_pengunjung,pesan) values ('$nama_pengunjung','$pesan')";
+$sql = "insert into data_inputan (nama_pengunjung,pesan,last_name) values ('$nama_pengunjung','$pesan','$last_name')";
 
 //
 $hasil=mysqli_query($conn,$sql);
